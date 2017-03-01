631090ae46b0c986d317f783d66c0eeaeeb16aab
Nate Dillon <natedillon@gmail.com>
2017-03-01 10:45:36 -0600

    Added main.js file

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+/*
+ * static-boilerplate (https://github.com/natedillon/static-boilerplate)
+ * Copyright (c) 2017 D. Nathan Dillon
+ * Licensed under MIT (https://github.com/natedillon/static-boilerplate/blob/master/LICENSE)
+ */
