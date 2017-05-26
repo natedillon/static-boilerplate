69a26085dd299fff2c6eec52b1788de6ddf8db83
Nate Dillon <natedillon@gmail.com>
2017-05-26 14:39:03 -0500

    Updated Grunt Sass settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,10 +14,7 @@ module.exports = function(grunt) {
     sass: {
       options: {
         sourceMap: true,
-        relativeAssets: false,
         outputStyle: 'expanded',
-        sassDir: 'src/css',
-        cssDir: 'css',
       },
       build: {
         files: [{
